44d18d3453ee17ae1a828adba9a9bcf1d93289bc
SaaranshDx <saaransh762@gmail.com>
2026-09-09 20:01:27 +0530

    fix: changed ytm loading from file to load from the url

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 1200,
-        height: 800,
+        width: 1325,
+        height: 870,
         webPreferences: {
             preload: path.join(__dirname, "preload.js")
         }
     });
 
-    win.loadFile("https://music.youtube.com/");
+    win.loadURL("https://music.youtube.com/");
 }
 
 app.whenReady().then(() => {
